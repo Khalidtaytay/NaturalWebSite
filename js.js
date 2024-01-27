bf914e5cc82ba7e61869b109dea3ba68d7666c28
Khalid <taytaykhalid2001@gmail.com>
2024-01-27 17:25:45 +0100

    css and js added

--- js.js
+++ js.js
@@ -1,5 +1,21 @@
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
+
 let backgrounds = document.querySelector('.backgrounds');
 let background123 = document.querySelectorAll('div[class*="background"]');
+let button = document.querySelector('.btn');
+
+//cover image effect
 
 let tl = gsap.timeline({
     scrollTrigger: {
@@ -16,3 +32,31 @@ background123.forEach((bg) => {
         duration: 100,
     }, 0);
 });
+
+//scroll dow
+document.querySelector('.btn').addEventListener('click', function() {
+    // Scroll down to 70% of the page height with slow and smooth effect
+    const targetPercentage = 0.267;
+    const targetPosition = document.body.scrollHeight * targetPercentage;
+    slowScroll(targetPosition, 1500);
+});
+
+function slowScroll(targetPosition, duration) {
+    const startPosition = window.scrollY;
+    const startTime = performance.now();
+
+    function animateScroll(currentTime) {
+        const elapsed = currentTime - startTime;
+        const progress = Math.min(elapsed / duration, 1);
+        const newPosition = startPosition + (targetPosition - startPosition) * progress;
+
+        window.scrollTo(0, newPosition);
+
+        if (progress < 1) {
+            requestAnimationFrame(animateScroll);
+        }
+    }
+
+    requestAnimationFrame(animateScroll);
+}
+button.addEventListener('click',scrolldown)
